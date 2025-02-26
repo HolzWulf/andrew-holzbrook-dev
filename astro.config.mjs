8d50b97e771dc0a8669a4cdd5ab92eecd6b8d5d6
Andrew Holbrook <holz55@gmail.com>
2025-02-26 01:46:47 -0700

    trim things down

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import netlify from '@astrojs/netlify';
-
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
