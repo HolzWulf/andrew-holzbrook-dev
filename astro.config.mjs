7776218d5d745dcc595cb3d3a68b0e7d0ae7753b
Andrew Holbrook <holz55@gmail.com>
2025-02-26 01:17:16 -0700

    another attempt to fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,21 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import netlify from '@astrojs/netlify';
 import solidJs from "@astrojs/solid-js";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: netlify(),
+  site: 'https://holzbrook.dev',
   output: 'static',
-  outDir: './build',
-  integrations: [solidJs({ devtools: true })],
+  outDir: './dist',
+  cacheDir: './cache',
+  integrations: [
+    solidJs({ devtools: true }),
+  ],
   vite: {
     clearScreen: false,
     plugins: [tailwindcss()],
